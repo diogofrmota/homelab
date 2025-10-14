9e8b73ef3500f0da168cc339a33eb010f640f8f3
diogofrmota <diogofrmota@gmail.com>
2025-10-14 17:57:27 +0100

    update homepage

--- scripts/cluster_restart.sh
+++ scripts/cluster_restart.sh
@@ -1,26 +1,24 @@
-# Restart all ArgoCD deployments
+# Restart all Kube-system deployments
+kubectl -n kube-system rollout restart deployment coredns local-path-provisioner metrics-server
+
+# Restart all ArgoCD deployments and statefullset
 kubectl -n argocd rollout restart deployment argocd-applicationset-controller
 kubectl -n argocd rollout restart deployment argocd-dex-server
 kubectl -n argocd rollout restart deployment argocd-notifications-controller
 kubectl -n argocd rollout restart deployment argocd-redis
 kubectl -n argocd rollout restart deployment argocd-repo-server
 kubectl -n argocd rollout restart deployment argocd-server
-
-# Restart the statefulset
 kubectl -n argocd rollout restart statefulset argocd-application-controller
+
+# Restart all Metallb deployments
+kubectl -n metallb rollout restart deployment metallb-controller
+kubectl -n metallb rollout restart daemonset metallb-speaker
+
+# Restart all Ingress Nginx deployments
+kubectl -n ingress-nginx rollout restart deployment ingress-nginx-controller
 
 # Restart all Cert-manager deployments
 kubectl -n cert-manager rollout restart deployment cert-manager cert-manager-cainjector cert-manager-webhook
 
 # Restart all Homepage deployments
 kubectl -n homepage rollout restart deployment homepage
-
-# Restart all Ingress Nginx deployments
-kubectl -n ingress-nginx rollout restart deployment ingress-nginx-controller
-
-# Restart all Metallb deployments
-kubectl -n metallb rollout restart deployment metallb-controller
-kubectl -n metallb rollout restart daemonset metallb-speaker
-
-# Restart all Kube-system deployments
-kubectl -n kube-system rollout restart deployment coredns local-path-provisioner metrics-server
